4ea694b2fe6d9843004f73be1fa8d24714cb0de0
Julian Boyko <julian.boyko@outlook.com>
2017-04-11 13:51:00 -0400

    Testing Commiting on Temrinal

--- Project Supreme/SupremeUtilities.swift
+++ Project Supreme/SupremeUtilities.swift
@@ -14,6 +14,7 @@ import UIKit
 
 struct awsErrorType {
     
+    static let test = "Testing"
     static let timedOut = "errorMessage"
     
     // error codes matched to their corresponding errors
